79dfd6b2ea3a1fff21ec4f55bf2aa2b856c2efb0
yanlele <331393627@qq.com>
2024-04-27 17:17:09 +0800

    feat: juejin 深度阅读模式

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,5 +1,3 @@
-console.log(`[yanle] - yanle background`);
-
 // chrome.runtime.onInstalled 扩展程序加载的时候
 chrome.runtime.onInstalled.addListener((tab) => {
   console.log(`[yanle] - tab`, tab);
@@ -9,34 +7,28 @@ chrome.runtime.onInstalled.addListener((tab) => {
   });
 });
 
-const extensions = "https://developer.chrome.com/docs/extensions";
-const webstore = "https://developer.chrome.com/docs/webstore";
-
+// 点击插件图标
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab?.url?.startsWith(extensions) || tab?.url?.startsWith(webstore)) {
-    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === "ON" ? "OFF" : "ON";
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  const nextState = prevState === "ON" ? "OFF" : "ON";
 
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
+  // 切换徽标描述文本
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
+  });
+
+  if (nextState === "ON") {
+    // 插入 css
+    await chrome.scripting.insertCSS({
+      files: ["background/focus-mode.css"],
+      target: { tabId: tab.id as number },
     });
-
-    if (nextState === "ON") {
-      // Insert the CSS file when the user turns the extension on
-      await chrome.scripting.insertCSS({
-        files: ["background/focus-mode.css"],
-        target: { tabId: tab.id as number },
-      });
-    } else if (nextState === "OFF") {
-      // Remove the CSS file when the user turns the extension off
-      await chrome.scripting.removeCSS({
-        files: ["background/focus-mode.css"],
-        target: { tabId: tab.id as number },
-      });
-    }
+  } else if (nextState === "OFF") {
+    // 移除 css
+    await chrome.scripting.removeCSS({
+      files: ["background/focus-mode.css"],
+      target: { tabId: tab.id as number },
+    });
   }
 });
