f879af51ea4835ec54e20ed37ac05968520fdc8b
yanlele <331393627@qq.com>
2024-04-27 13:11:40 +0800

    feat: 添加 content 逻辑

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,25 +1,32 @@
-const article = document.querySelector("article");
+const $article = document.getElementById("article-root");
 
-console.log(`[yanle] - 1234`);
+if ($article) {
+  const wordLength = $article.innerText.length;
+  const $imgs = $article.querySelectorAll("img");
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent as string;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const $container = document.createElement("div");
 
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
+  const $wordsElement = document.createElement("p");
+  $wordsElement.innerText = `统计该文章字符：共计 ${wordLength} 个字符`;
+  $container.append($wordsElement);
 
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode as HTMLElement;
+  const $desc = (document.createElement("p").innerText = "提取该文章的所有图片");
+  $container.append($desc);
 
-  (date ?? heading).insertAdjacentElement("afterend", badge);
+  for (const img of $imgs) {
+    const src = img.getAttribute("src");
+    if (src) {
+      const $p = document.createElement("p");
+      const $a = document.createElement("a");
+      $a.setAttribute("href", src);
+      $a.setAttribute("target", "_blank");
+      $a.innerText = src;
+
+      $p.append($a);
+
+      $container.append($p);
+    }
+  }
+
+  document.getElementsByClassName("author-info-block")?.[0]?.insertAdjacentElement("afterend", $container);
 }
